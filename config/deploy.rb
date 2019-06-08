e3149bf5ed1fbd05cd2a9897d37b3c9f6056ff60
Norimitsu-Shimizu <sparesound31@gmail.com>
2019-06-08 14:26:40 +0900

    change copy-branch 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ lock "~> 3.11.0"
 set :application, "chat-space"
 set :repo_url, "git@github.com:Norimitsu-Shimizu/chat-space.git"
 
+set :branch, 'copy-branch'
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
